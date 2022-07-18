ded3c1ec42b405262493b4ab2eaf301aa853cb2a
kushalkumarkain <kushalkumar.kain156@gmail.com>
2022-07-18 15:42:59 +0530

    updated code for rerun and linking files

--- main.py
+++ main.py
@@ -1,20 +1,20 @@
 while True:
-  option= int(input("Choose any one function :",
-                "1. Fibonacci Sequence",
-                "2. Armstrong Number",
-                "3. Prime Number"))
-  
-  
-  
-
+  option= int(input('''
+  1. Fibonacci Sequence 
+  2. Armstrong Number 
+  3. Prime Number
+  Choose any one function : '''))
   if option == 1:
     print("You selected Fibonacci number")
+    from .options import fibonacci
 
   elif option == 2:
     print("You selected Armstrong number")
-    
+    from .options import armstrong
+        
   elif option == 3:
     print("You selected Prime number check")
+    from .options import prime
     
   else:
     print("Enter valid input and try again.")
