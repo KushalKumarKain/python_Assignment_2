403c2e7ae6c1e85db2a42347f1feaa3b594a346c
kushalkumarkain <kushalkumar.kain156@gmail.com>
2022-07-18 16:54:40 +0530

    updated logic code for fibonacci.

--- options/fibonacci.py
+++ options/fibonacci.py
@@ -4,26 +4,26 @@ from base import main_abstract
 number = int(input("Enter number of choice : "))
 class fibo(main_abstract):
    def run(self, number):
+            
+         n1, n2 = 0, 1
+         count = 0
 
-      n1 = 0
-      n2 = 1
-
-      if number < 0:
-         print("Please enter a positive integer")
+         if number <= 0:
+            print("Please enter a positive integer")
          
-      elif number == 0:
-         return n2
-      
-      # generate fibonacci sequence
-      else:
-         print("Fibonacci sequence:")
-         while n1 < number:
+         elif number == 1:
+            print("Fibonacci sequence upto",number,":")
             print(n1)
-            nth = n1 + n2
-            # update values
-            n1 = n2
-            n2 = nth
-            n1 += 1
+         # generate fibonacci sequence
+         else:
+            print("Fibonacci sequence:")
+            while count < number:
+               print(n1)
+               nth = n1 + n2
+               # update values
+               n1 = n2
+               n2 = nth
+               count += 1
 
 F = fibo()
 F.run(number)
