f51faf687aabaed48e6ee1ce1b3a5e4b9851265a
kushalkumarkain <kushalkumar.kain156@gmail.com>
2022-07-18 16:25:01 +0530

    changed the order to run file

--- base.py
+++ base.py
@@ -1,18 +1,16 @@
-import main
-
 from abc import ABC,abstractmethod
 
-class fibonacci(ABC):
+class main_abstract(ABC):
    @abstractmethod
    def run(self):
       pass
     
-class PRIME(ABC):
-   @abstractmethod
-   def run(self):
-      pass
+# class PRIME(ABC):
+#    @abstractmethod
+#    def run(self):
+#       pass
     
-class armstrong(ABC):
-   @abstractmethod
-   def run(self):
-      pass
+# class armstrong(ABC):
+#    @abstractmethod
+#    def run(self):
+#       pass
--- main.py
+++ main.py
@@ -1,20 +1,22 @@
+import options
 while True:
   option= int(input('''
   1. Fibonacci Sequence 
   2. Armstrong Number 
   3. Prime Number
   Choose any one function : '''))
+  
   if option == 1:
     print("You selected Fibonacci number")
-    from .options import fibonacci
+    from options import fibonacci
 
   elif option == 2:
     print("You selected Armstrong number")
-    from .options import armstrong
+    from options import armstrong
         
   elif option == 3:
     print("You selected Prime number check")
-    from .options import prime
+    from options import prime
     
   else:
     print("Enter valid input and try again.")
