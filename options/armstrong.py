35f5a874b35d2f61024f4b4e8b611f24d85e3a75
kushalkumarkain <kushalkumar.kain156@gmail.com>
2022-07-18 13:07:53 +0530

    uploaded code for armstrong number

---
+++ options/armstrong.py
@@ -0,0 +1,27 @@
+from abc import ABC,abstractmethod
+import numbers
+
+class armstrong(ABC):
+   @abstractmethod
+   def run(self):
+      pass
+number = int(input("Enter number of choice : "))
+class arm(armstrong):
+   def run(self, number):
+     
+    sum = 0
+
+    temp = number
+    while temp > 0:
+      digit = temp % 10
+      sum += digit ** 3
+      temp //= 10
+
+    # display the result
+    if number == sum:
+      print(number,"is an Armstrong number")
+    else:
+      print(number,"is not an Armstrong number")
+      
+A = arm()
+A.run(number)
