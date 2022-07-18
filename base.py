d37797706a8b49c549e7a0e00709b199a9059b0e
kushalkumarkain <kushalkumar.kain156@gmail.com>
2022-07-18 15:42:20 +0530

    uploaded code for base file

--- base.py
+++ base.py
@@ -1,2 +1,18 @@
 import main
 
+from abc import ABC,abstractmethod
+
+class fibonacci(ABC):
+   @abstractmethod
+   def run(self):
+      pass
+    
+class PRIME(ABC):
+   @abstractmethod
+   def run(self):
+      pass
+    
+class armstrong(ABC):
+   @abstractmethod
+   def run(self):
+      pass
