2585c222a2cbeb0c59fba88e429cd7fc83771582
kushalkumarkain <kushalkumar.kain156@gmail.com>
2022-07-18 14:37:51 +0530

    updated code for import and selection

---
+++ main.py
@@ -0,0 +1,20 @@
+while True:
+  option= int(input("Choose any one function :",
+                "1. Fibonacci Sequence",
+                "2. Armstrong Number",
+                "3. Prime Number"))
+  
+  
+  
+
+  if option == 1:
+    print("You selected Fibonacci number")
+
+  elif option == 2:
+    print("You selected Armstrong number")
+    
+  elif option == 3:
+    print("You selected Prime number check")
+    
+  else:
+    print("Enter valid input and try again.")
