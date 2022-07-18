0141553abb953264a861b5ef5fe2c0ddf79d23bd
kushalkumarkain <kushalkumar.kain156@gmail.com>
2022-07-18 16:34:13 +0530

    removed commemted code

--- base.py
+++ base.py
@@ -4,13 +4,3 @@ class main_abstract(ABC):
    @abstractmethod
    def run(self):
       pass
-    
-# class PRIME(ABC):
-#    @abstractmethod
-#    def run(self):
-#       pass
-    
-# class armstrong(ABC):
-#    @abstractmethod
-#    def run(self):
-#       pass
